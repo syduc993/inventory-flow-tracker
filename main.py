bbd287ec90299c1d606a434e44a8d9916487d6bb
syduc993 <le.syduc.993@gmail.com>
2025-08-11 16:47:08 +0700

    Update

--- main.py
+++ main.py
@@ -199,4 +199,7 @@ async def refresh_transport_providers_endpoint():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    import os
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
+
